a06078e211e32913cf515f5a96a3f69867341dcf
Jlixerkun <jlixerkun@gmail.com>
2016-07-01 23:44:23 -0500

    incluye fontawesome y añade cambiios

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="scss/font-awesome.css" />
+  <link rel="stylesheet" href="css/estilo.css" />
+  <title>Futuro | Economía solidaria</title>
+
+</head>
+
+<body>
+  <div class="page">
+    <header class="site-header">
+      <div class="container">
+        
+        
+        <section class="flex-row-sbtwn">
+          <div class="brand">
+            <h1 class="site-title">Futuro</h1>
+            <span class="site-tagline">Economía solidaria para todos</span>
+          </div>
+          <nav class="top-nav">
+            <ul class="menu">
+              <li><a href="#">Convenios</a></li>
+              <li><a href="#">Simuladores</a></li>
+              <li><a href="#">Afíliese</a></li>
+              <li><a href="#">Contáctenos</a></li>
+              <li><a href="#"><i class="fa fa-child fa-2x"></i> Zona de afiliados</a></li>
+            </ul>
+          </nav>
+        </section>
+        
+        
+        
+        
+      </div>
+    </header>
+    <section class="content">
+      
+      <div class="banner b1 flex-center-both">
+        <div class="container">
+            <h2 class="text-center">Asegúrese el mejor futuro</h2>
+            <p class="text-center">para quienes importan más</p>
+        </div>
+      </div>
+      
+    </section>
+    <footer class="site-footer">
+    </footer>
+  </div>
+
+
+</body>
+
+</html>
